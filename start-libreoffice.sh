ae696f431aebbfe6f89f722fec80d1685a00a54e
Sean Lee <leejoneshane@gmail.com>
2020-12-21 11:21:16 +0800

    Update start-libreoffice.sh

--- start-libreoffice.sh
+++ start-libreoffice.sh
@@ -6,32 +6,7 @@
 # Fix domain name resolution from jails
 cp /etc/resolv.conf /etc/hosts /opt/lool/systemplate/etc/
 
-if test "${DONT_GEN_SSL_CERT-set}" == set; then
-# Generate new SSL certificate instead of using the default
-mkdir -p /tmp/ssl/
-cd /tmp/ssl/
-mkdir -p certs/ca
-openssl rand -writerand /opt/lool/.rnd
-openssl genrsa -out certs/ca/root.key.pem 2048
-openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority"
-mkdir -p certs/{servers,tmp}
-mkdir -p certs/servers/localhost
-openssl genrsa -out certs/servers/localhost/privkey.pem 2048
-if test "${cert_domain-set}" == set; then
-openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost"
-else
-openssl req -key certs/servers/localhost/privkey.pem -new -sha256 -out certs/tmp/localhost.csr.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=${cert_domain}"
-fi
-openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131
-mv certs/servers/localhost/privkey.pem /etc/loolwsd/key.pem
-mv certs/servers/localhost/cert.pem /etc/loolwsd/cert.pem
-mv certs/ca/root.crt.pem /etc/loolwsd/ca-chain.cert.pem
-fi
-
-# Disable warning/info messages of LOKit by default
-if test "${SAL_LOG-set}" == set; then
 SAL_LOG="-INFO-WARN"
-fi
 
 # Backward compatible way to pass configuration settings with environment variables
 # These environemt variables are documented at various places so we keep support of them.
